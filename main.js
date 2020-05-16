ba2b15da775dd39eb0acbd1dddeca0e1bc700d44
Julia Zamaitat <j.zamaitat@gmail.com>
2020-05-16 21:39:03 +0200

    Little fixes and install of eslint.

--- main.js
+++ main.js
@@ -2,22 +2,19 @@
 
 //Require the necessary modules
 const express = require("express"),
-  app = express(),
-  layouts = require("express-ejs-layouts"),
-  boardsController = require("./controllers/boardsController"),
-  mongoose = require("mongoose"),
-  bodyParser = require('body-parser'),
-  http = require('http'),
-  io = require('socket.io').listen(server),
-  Card = require('./models/card');
-
-//Tell node to use promises with mongoose
-mongoose.Promise = global.Promise;
+	app = express(),
+	layouts = require("express-ejs-layouts"),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser"),
+	http = require("http"),
+	server = http.createServer(app),
+	io = require("socket.io").listen(server),
+	Card = require("./models/card");
 
 //Connects either to the procution database, docker db or our local database
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/board_db", 
-  { useNewUrlParser: true, useFindAndModify: false }
+	process.env.MONGODB_URI || "mongodb://localhost:27017/board_db",
+	{ useNewUrlParser: true, useFindAndModify: false }
 );
 
 //Get a db instance to work with
@@ -25,14 +22,14 @@ const db = mongoose.connection;
 
 //Notfication when connection to db was successfull
 db.once("open", () => {
-  console.log("Connected to MongoDB");
+	console.log("Connected to MongoDB");
 });
 
 //In order to parse JSON for our application
 app.use(
-    express.urlencoded({
-        extended: true
-    })
+	express.urlencoded({
+		extended: true
+	})
 );
 app.use(express.json());
 
@@ -48,11 +45,12 @@ app.use(bodyParser.json());
 
 //Start listening to the PORT
 app.listen(app.get("port"), () => {
-  console.log(`Server running at http://localhost:${app.get("port")}`);
+	console.log(`Server running at http://localhost:${app.get("port")}`);
+});
 
 
-io.on('connection', () => {
-    console.log('A new user is connected')
+io.on("connection", () => {
+	console.log("A new user is connected");
 });
 
 // Routes
@@ -64,52 +62,49 @@ app.post("/", save_card);
 // Controller
 // TODO: export with socket.io
 function save_card(req, res) {
-    const card = new Card(
-        {
-            _id: new mongoose.mongo.ObjectId(),
-            backgroundColor: req.body.color,
-            position: {
-                left: null,
-                top: null
-            },
-        }
-    );
-    card.save((err) => {
-        if (err)
-            sendStatus(500);
-        io.emit('new-card', JSON.stringify(card));
-        res.sendStatus(200);
+	const card = new Card(
+		{
+			_id: new mongoose.mongo.ObjectId(),
+			backgroundColor: req.body.color,
+			position: {
+				left: null,
+				top: null
+			}
+		});
+	card.save((err) => {
+		if (err)
+			res.sendStatus(500);
+		io.emit("new-card", JSON.stringify(card));
+		res.sendStatus(200);
 
-    })
+	});
 }
 
-function get_cards_data(req, res) {
-    Card.find({}, (err, card) => {
-        res.send(card);
-    })
+function get_cards_data (req, res) {
+	Card.find({}, (err, card) => {
+		res.send(card);
+	});
 }
 
-function update_card(req, res) {
+function update_card (req, res) {
 
-    const filter = {_id: mongoose.Types.ObjectId(req.body._id)};
-    const update = {position: {left: req.body.position.left, top: req.body.position.top}};
+	const filter = {_id: mongoose.Types.ObjectId(req.body._id)};
+	const update = {position: {left: req.body.position.left, top: req.body.position.top}};
 
-    Card.findOneAndUpdate(filter, update, {new: true},
-        function (err) {
-            if (err) {
-                console.log("Something wrong when updating data!");
-            }
-        });
+	Card.findOneAndUpdate (filter, update, {new: true},
+		function (err) {
+			if (err) console.log("Something wrong when updating data!");
+		});
 
-    io.emit('pos-update', JSON.stringify({
-        _id: req.body._id,
-        position: {
-            left: req.body.position.left,
-            top: req.body.position.top
-        }
-    }));
+	io.emit("pos-update", JSON.stringify({
+		_id: req.body._id,
+		position: {
+			left: req.body.position.left,
+			top: req.body.position.top
+		}
+	}));
 }
 
-function get_cards(req, res) {
-    res.render("boards/index");
+function get_cards (req, res) {
+	res.render("boards/index");
 }
