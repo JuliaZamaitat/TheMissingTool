46307eaba2df07e5e92683a1ac49e10c8a7645be
akeller <annina.keller@visablelabs.com>
2020-05-25 13:12:18 +0200

    use websockets instead of endpoints and rooms and only emit text to not sender

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 
 //Require the necessary modules
 const express = require("express"),
-	app = express(),
-	layouts = require("express-ejs-layouts"),
-	mongoose = require("mongoose"),
-	bodyParser = require("body-parser"),
-	http = require("http"),
-	server = http.Server(app),
-	io = require("socket.io")(server),
-	Card = require("./models/card");
+    app = express(),
+    layouts = require("express-ejs-layouts"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    http = require("http"),
+    server = http.Server(app),
+    io = require("socket.io")(server),
+    Card = require("./models/card");
 
 //Connects either to the procution database, docker db or our local database
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://localhost:27017/board_db",
-	{useNewUrlParser: true, useFindAndModify: false}
+    process.env.MONGODB_URI || "mongodb://localhost:27017/board_db",
+    {useNewUrlParser: true, useFindAndModify: false}
 );
 
 //Get a db instance to work with
@@ -22,14 +22,14 @@ const db = mongoose.connection;
 
 //Notfication when connection to db was successfull
 db.once("open", () => {
-	console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB");
 });
 
 //In order to parse JSON for our application
 app.use(
-	express.urlencoded({
-		extended: true
-	})
+    express.urlencoded({
+        extended: true
+    })
 );
 app.use(bodyParser.json());
 app.use(express.json());
@@ -42,7 +42,7 @@ app.set("view engine", "ejs");
 app.set("port", process.env.NODEPORT || process.env.PORT || 8080);
 
 server.listen(app.get("port"), () => {
-	console.log(`Server running at http://localhost:${app.get("port")}`);
+    console.log(`Server running at http://localhost:${app.get("port")}`);
 });
 
 // Routes
@@ -51,97 +51,97 @@ app.get("/board/:boardId", get_cards);
 
 // Controller
 function get_board(req, res) {
-	res.render("boards/index");
+    res.render("boards/index");
 }
 
 function get_cards(req, res) {
-	let boardId = req.params.boardId;
-	Card.find({boardId: boardId}, (err, cards) => {
-		res.send(cards);
-	});
+    let boardId = req.params.boardId;
+    Card.find({boardId: boardId}, (err, cards) => {
+        res.send(cards);
+    });
 }
 
 module.exports = app;
 
 io.on('connection', function (socket) {
-	let board;
-	console.log('a user connected with id %s', socket.id);
+    let board;
+    console.log('a user connected with id %s', socket.id);
 
-	socket.on('join', function (room) {
-		socket.join(room);
-		board = room;
-		console.log(socket.id, "joined", room);
-	});
+    socket.on('join', function (room) {
+        socket.join(room);
+        board = room;
+        console.log(socket.id, "joined", room);
+    });
 
-	socket.on('save-card', function (req) {
-		const card = new Card(
-			{
-				_id: new mongoose.mongo.ObjectId(),
-				backgroundColor: req.color,
-				position: {
-					left: null,
-					top: null
-				},
-				boardId: board
-			});
-		card.save((err) => {
-			if (err) {
-				console.log("Something wrong saving card");
-			}
-			io.to(board).emit("new-card", JSON.stringify(card));
-		});
-	});
+    socket.on('save-card', function (req) {
+        const card = new Card(
+            {
+                _id: new mongoose.mongo.ObjectId(),
+                backgroundColor: req.color,
+                position: {
+                    left: null,
+                    top: null
+                },
+                boardId: board
+            });
+        card.save((err) => {
+            if (err) {
+                console.log("Something wrong saving card");
+            }
+            io.to(board).emit("new-card", JSON.stringify(card));
+        });
+    });
 
-	socket.on('update-pos', function (req) {
-		const filter = {_id: mongoose.Types.ObjectId(req._id)};
-		const update = {position: {left: req.position.left, top: req.position.top}};
+    socket.on('update-pos', function (req) {
+        const filter = {_id: mongoose.Types.ObjectId(req._id)};
+        const update = {position: {left: req.position.left, top: req.position.top}};
 
-		Card.findOneAndUpdate(filter, update, {new: true},
-			function (err) {
-				if (err) {
-					console.log("Something wrong when updating data!");
-				}
-				io.to(board).emit("pos-update", JSON.stringify({
-					_id: req._id,
-					position: {
-						left: req.position.left,
-						top: req.position.top
-					}
-				}));
-			}
-		);
-	});
+        Card.findOneAndUpdate(filter, update, {new: true},
+            function (err) {
+                if (err) {
+                    console.log("Something wrong when updating data!");
+                }
+                io.to(board).emit("pos-update", JSON.stringify({
+                    _id: req._id,
+                    position: {
+                        left: req.position.left,
+                        top: req.position.top
+                    }
+                }));
+            }
+        );
+    });
 
-	socket.on('delete-card', function (req) {
-		const filter = {_id: mongoose.Types.ObjectId(req._id)};
+    socket.on('delete-card', function (req) {
+        const filter = {_id: mongoose.Types.ObjectId(req._id)};
 
-		Card.deleteOne(filter,
-			function (err) {
-				if (err) {
-					console.log("Something wrong when deleting data!");
-				}
-				io.to(board).emit("delete-card", JSON.stringify({
-					_id: req._id
-				}));
-			}
-		);
-	});
+        Card.deleteOne(filter,
+            function (err) {
+                if (err) {
+                    console.log("Something wrong when deleting data!");
+                }
+                io.to(board).emit("delete-card", JSON.stringify({
+                    _id: req._id
+                }));
+            }
+        );
+    });
 
-	socket.on('update-text', function (req) {
-		const filter = {_id: mongoose.Types.ObjectId(req._id)};
-		const update = {text: req.text};
+    socket.on('update-text', function (req) {
+        const filter = {_id: mongoose.Types.ObjectId(req._id)};
+        const update = {text: req.text};
 
-		Card.findOneAndUpdate(filter, update, {new: true},
-			function (err) {
-				if (err) {
-					console.log("Something wrong when updating data!");
-				}
-				socket.broadcast.to(board).emit("text-update", JSON.stringify({
-					_id: req._id,
-					text: req.text
-				}));
-			}
-		);
-	});
+        Card.findOneAndUpdate(filter, update, {new: true},
+            function (err) {
+                if (err) {
+                    console.log("Something wrong when updating data!");
+                }
+                socket.broadcast.to(board).emit("text-update", JSON.stringify({
+                    _id: req._id,
+                    text: req.text
+                }));
+            }
+        );
+    });
 });
 
--- public/script/web_sockets.js
+++ public/script/web_sockets.js
@@ -4,126 +4,125 @@ var socket = io('localhost:3034');
 socket.emit("join", windowBoardId);
 
 window.onload = function () {
-	$.get('/board/' + windowBoardId, (cards) => {
-		cards.forEach(createCard);
-	})
+    $.get('/board/' + windowBoardId, (cards) => {
+        cards.forEach(createCard);
+    })
 };
 
 function createCard(data) {
-	const card = document.createElement('div');
-	card.className = 'item animate';
-	card.innerHTML = "<span type='button' class='deleteBtn rounded'><i class='fa fa-trash-o'></i></span><textarea type='text' value=''></textarea>";
-	card.id = data._id;
-	card.style.left = data.position.left + 'px';
-	card.style.top = data.position.top + 'px';
-	card.style.fontSize = data.fontSize;
-	card.style.backgroundColor = data.backgroundColor;
-	if (data.text != null) {
-		card.querySelector("textarea").value = data.text; //Show the card text if defined
-	}
-	addListeners(card);
-	document.getElementById('overlay').appendChild(card)
+    const card = document.createElement('div');
+    card.className = 'item animate';
+    card.innerHTML = "<span type='button' class='deleteBtn rounded'><i class='fa fa-trash-o'></i></span><textarea type='text' value=''></textarea>";
+    card.id = data._id;
+    card.style.left = data.position.left + 'px';
+    card.style.top = data.position.top + 'px';
+    card.style.fontSize = data.fontSize;
+    card.style.backgroundColor = data.backgroundColor;
+    if (data.text != null) {
+        card.querySelector("textarea").value = data.text; //Show the card text if defined
+    }
+    addListeners(card);
+    document.getElementById('overlay').appendChild(card)
 }
 
 function addListeners(card) {
+    // Moving card listener
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    card.onmousedown = cardMouseDown;
 
-	// Moving card listener
-	let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-	card.onmousedown = cardMouseDown;
+    function cardMouseDown(e) {
+        card.classList.remove("animate");
+        card.style.position = "absolute";
+        card.style.zIndex = 1000;
+        document.getElementById('overlay').append(card);
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragCard;
+        document.onmousemove = dragCard;
+    }
 
-	function cardMouseDown(e) {
-		card.classList.remove("animate");
-		card.style.position = "absolute";
-		card.style.zIndex = 1000;
-		document.getElementById('overlay').append(card);
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		document.onmouseup = closeDragCard;
-		document.onmousemove = dragCard;
-	}
+    function dragCard(e) {
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        card.style.top = (card.offsetTop - pos2) + "px";
+        card.style.left = (card.offsetLeft - pos1) + "px";
+    }
 
-	function dragCard(e) {
-		e.preventDefault();
-		pos1 = pos3 - e.clientX;
-		pos2 = pos4 - e.clientY;
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		card.style.top = (card.offsetTop - pos2) + "px";
-		card.style.left = (card.offsetLeft - pos1) + "px";
-	}
+    function closeDragCard() {
+        sendPosChange({
+            _id: card.id,
+            position: {
+                left: card.style.left.replace(/\D/g, ''),
+                top: card.style.top.replace(/\D/g, ''),
+            }
+        });
+        document.onmouseup = null;
+        document.onmousemove = null;
 
-	function closeDragCard() {
-		sendPosChange({
-			_id: card.id,
-			position: {
-				left: card.style.left.replace(/\D/g, ''),
-				top: card.style.top.replace(/\D/g, ''),
-			}
-		});
-		document.onmouseup = null;
-		document.onmousemove = null;
+    }
 
-	}
+    function sendPosChange(update) {
+        socket.emit('update-pos', update);
+    }
 
-	function sendPosChange(update) {
-		socket.emit('update-pos', update);
-	}
+    card.ondragstart = function () {
+        return false;
+    };
 
-	card.ondragstart = function () {
-		return false;
-	};
+    // Delete card listener
+    card.querySelector('.deleteBtn').addEventListener('mousedown', function (event) {
+        event.stopPropagation();  //prevent bubbling process so the whole card doesn't start dragging
+        const cardToDelete = {_id: event.currentTarget.parentElement.id};
+        socket.emit('delete-card', cardToDelete);
+    });
 
-	// Delete card listener
-	card.querySelector('.deleteBtn').addEventListener('mousedown', function (event) {
-		event.stopPropagation();  //prevent bubbling process so the whole card doesn't start dragging
-		const cardToDelete = {_id: event.currentTarget.parentElement.id};
-		socket.emit('delete-card', cardToDelete);
-	});
-
-	// Change text of card listener
-	card.querySelector('textarea').addEventListener('input', function (event) {
-		socket.emit('update-text', {
-			_id: event.currentTarget.parentElement.id,
-			text: event.currentTarget.value
-		})
-	});
+    // Change text of card listener
+    card.querySelector('textarea').addEventListener('input', function (event) {
+        socket.emit('update-text', {
+            _id: event.currentTarget.parentElement.id,
+            text: event.currentTarget.value
+        })
+    });
 }
 
 // event listener for plus button
 $("#plus").click(() => {
-	socket.emit('save-card', {
-		color: getRandomColor()
-	});
+    socket.emit('save-card', {
+        color: getRandomColor()
+    });
 });
 
 // listening to web-sockets
 socket.on('new-card', (data) => {
-	const card = JSON.parse(data);
-	createCard(card)
+    const card = JSON.parse(data);
+    createCard(card)
 });
 
 socket.on('pos-update', (data) => {
-	const card = JSON.parse(data);
-	let cardById = document.getElementById(card._id);
-	cardById.style.left = card.position.left + 'px';
-	cardById.style.top = card.position.top + 'px';
+    const card = JSON.parse(data);
+    let cardById = document.getElementById(card._id);
+    cardById.style.left = card.position.left + 'px';
+    cardById.style.top = card.position.top + 'px';
 });
 
 socket.on('text-update', (data) => {
-	const card = JSON.parse(data);
-	$('#' + card._id).find('textarea').val(card.text);
+    const card = JSON.parse(data);
+    $('#' + card._id).find('textarea').val(card.text);
 });
 
 socket.on('delete-card', (data) => {
-	const card = JSON.parse(data);
-	$('#' + card._id).remove(); //remove the card element by its ID
+    const card = JSON.parse(data);
+    $('#' + card._id).remove(); //remove the card element by its ID
 });
 
 function getRandomColor() {
-	var letters = '0123456789ABCDEF';
-	var color = '#';
-	for (var i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
