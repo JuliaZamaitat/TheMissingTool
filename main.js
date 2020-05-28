bfd8abaadf914d15f621ab98f9677269a0045d17
Julia Zamaitat <j.zamaitat@gmail.com>
2020-05-28 10:40:17 +0200

    Dynamic port for client side websocket.

--- main.js
+++ main.js
@@ -2,20 +2,20 @@
 
 //Require the necessary modules
 const express = require("express"),
-    app = express(),
-    layouts = require("express-ejs-layouts"),
-    mongoose = require("mongoose"),
-    bodyParser = require("body-parser"),
-    http = require("http"),
-    server = http.Server(app),
-    io = require("socket.io")(server),
-    Card = require("./models/card"),
-    Board = require("./models/board");
+	app = express(),
+	layouts = require("express-ejs-layouts"),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser"),
+	http = require("http"),
+	server = http.Server(app),
+	io = require("socket.io")(server),
+	Card = require("./models/card"),
+	Board = require("./models/board");
 
 //Connects either to the procution database, docker db or our local database
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/board_db",
-    {useNewUrlParser: true, useFindAndModify: false}
+	process.env.MONGODB_URI || "mongodb://localhost:27017/board_db",
+	{useNewUrlParser: true, useFindAndModify: false}
 );
 
 //Get a db instance to work with
@@ -23,14 +23,14 @@ const db = mongoose.connection;
 
 //Notfication when connection to db was successfull
 db.once("open", () => {
-    console.log("Connected to MongoDB");
+	console.log("Connected to MongoDB");
 });
 
 //In order to parse JSON for our application
 app.use(
-    express.urlencoded({
-        extended: true
-    })
+	express.urlencoded({
+		extended: true
+	})
 );
 app.use(bodyParser.json());
 app.use(express.json());
@@ -43,7 +43,7 @@ app.set("view engine", "ejs");
 app.set("port", process.env.NODEPORT || process.env.PORT || 8080);
 
 server.listen(app.get("port"), () => {
-    console.log(`Server running at http://localhost:${app.get("port")}`);
+	console.log(`Server running at http://localhost:${app.get("port")}`);
 });
 
 // Routes
@@ -52,158 +52,163 @@ app.post("/", create_board);
 app.get("/board/:boardId", get_board);
 app.get("/board/:boardId/cards", get_cards);
 
+app.get("/port", get_port);
+
+function get_port() {
+	return app.get("port");
+}
+
 // Controller
 function get_index(req, res) {
-    res.render("landing");
+	res.render("landing");
 }
 
 function create_board(req, res) {
-    var newBoard =
+	var newBoard =
         new Board({
-            _id: new mongoose.mongo.ObjectId(),
-            name: "The coolest board"
+        	_id: new mongoose.mongo.ObjectId(),
+        	name: "The coolest board"
         });
-    newBoard.save((err) => {
-        if (err) {
-            console.log("Error creating board");
-        } else {
-            res.send(newBoard._id);
-        }
-    })
+	newBoard.save((err) => {
+		if (err) {
+			console.log("Error creating board");
+		} else {
+			res.send(newBoard._id);
+		}
+	});
 }
 
 function get_board(req, res) {
-    const filter = {_id: mongoose.Types.ObjectId(req.params.boardId)};
-    Board.findOne(filter, (err, savedBoard) => {
-        res.render("boards/index", {board: savedBoard});
-    });
+	const filter = {_id: mongoose.Types.ObjectId(req.params.boardId)};
+	Board.findOne(filter, (err, savedBoard) => {
+		res.render("boards/index", {board: savedBoard});
+	});
 }
 
 function get_cards(req, res) {
-    const filter = {boardId: mongoose.Types.ObjectId(req.params.boardId)};
-    Card.find(filter, (err, cards) => {
-        res.send(cards);
-    });
+	const filter = {boardId: mongoose.Types.ObjectId(req.params.boardId)};
+	Card.find(filter, (err, cards) => {
+		res.send(cards);
+	});
 }
 
 module.exports = app;
 
-io.on('connection', function (socket) {
-    let board;
-    console.log('a user connected with id %s', socket.id);
+io.on("connection", function (socket) {
+	let board;
+	console.log("a user connected with id %s", socket.id);
 
-    socket.on('join', function (room) {
-        socket.join(room);
-        board = room;
-        console.log(socket.id, "joined", room);
-    });
+	socket.on("join", function (room) {
+		socket.join(room);
+		board = room;
+		console.log(socket.id, "joined", room);
+	});
 
-    socket.on('save-card', function (req) {
-        const card = new Card(
-            {
-                _id: new mongoose.mongo.ObjectId(),
-                backgroundColor: req.color,
-                position: {
-                    left: null,
-                    top: null
-                },
-                boardId: mongoose.Types.ObjectId(board)
-            });
-        card.save((err) => {
-            if (err) {
-                console.log("Something wrong saving card");
-            } else {
-                console.log(card._id, card.boardId);
-            }
-            io.to(board).emit("new-card", JSON.stringify(card));
-        });
-    });
+	socket.on("save-card", function (req) {
+		const card = new Card(
+			{
+				_id: new mongoose.mongo.ObjectId(),
+				backgroundColor: req.color,
+				position: {
+					left: null,
+					top: null
+				},
+				boardId: mongoose.Types.ObjectId(board)
+			});
+		card.save((err) => {
+			if (err) {
+				console.log("Something wrong saving card");
+			} else {
+				console.log(card._id, card.boardId);
+			}
+			io.to(board).emit("new-card", JSON.stringify(card));
+		});
+	});
 
-    socket.on('update-pos', function (req) {
-        const filter = {_id: mongoose.Types.ObjectId(req._id)};
-        const update = {position: {left: req.position.left, top: req.position.top}};
+	socket.on("update-pos", function (req) {
+		const filter = {_id: mongoose.Types.ObjectId(req._id)};
+		const update = {position: {left: req.position.left, top: req.position.top}};
 
-        Card.findOneAndUpdate(filter, update, {new: true},
-            function (err) {
-                if (err) {
-                    console.log("Something wrong when updating data!");
-                }
-                io.to(board).emit("pos-update", JSON.stringify({
-                    _id: req._id,
-                    position: {
-                        left: req.position.left,
-                        top: req.position.top
-                    }
-                }));
-            }
-        );
-    });
+		Card.findOneAndUpdate(filter, update, {new: true},
+			function (err) {
+				if (err) {
+					console.log("Something wrong when updating data!");
+				}
+				io.to(board).emit("pos-update", JSON.stringify({
+					_id: req._id,
+					position: {
+						left: req.position.left,
+						top: req.position.top
+					}
+				}));
+			}
+		);
+	});
 
-    socket.on('delete-card', function (req) {
-        const filter = {_id: mongoose.Types.ObjectId(req._id)};
+	socket.on("delete-card", function (req) {
+		const filter = {_id: mongoose.Types.ObjectId(req._id)};
 
-        Card.deleteOne(filter,
-            function (err) {
-                if (err) {
-                    console.log("Something wrong when deleting data!");
-                }
-                io.to(board).emit("delete-card", JSON.stringify({
-                    _id: req._id
-                }));
-            }
-        );
-    });
+		Card.deleteOne(filter,
+			function (err) {
+				if (err) {
+					console.log("Something wrong when deleting data!");
+				}
+				io.to(board).emit("delete-card", JSON.stringify({
+					_id: req._id
+				}));
+			}
+		);
+	});
 
-    socket.on('update-text', function (req) {
-        const filter = {_id: mongoose.Types.ObjectId(req._id)};
-        const update = {text: req.text};
+	socket.on("update-text", function (req) {
+		const filter = {_id: mongoose.Types.ObjectId(req._id)};
+		const update = {text: req.text};
 
-        Card.findOneAndUpdate(filter, update, {new: true},
-            function (err) {
-                if (err) {
-                    console.log("Something wrong when updating data!");
-                }
-                socket.broadcast.to(board).emit("text-update", JSON.stringify({
-                    _id: req._id,
-                    text: req.text
-                }));
-            }
-        );
-    });
+		Card.findOneAndUpdate(filter, update, {new: true},
+			function (err) {
+				if (err) {
+					console.log("Something wrong when updating data!");
+				}
+				socket.broadcast.to(board).emit("text-update", JSON.stringify({
+					_id: req._id,
+					text: req.text
+				}));
+			}
+		);
+	});
 
-    socket.on('update-board-name', function (req) {
-        const filter = {_id: mongoose.Types.ObjectId(req._id)};
-        const update = {name: req.name};
+	socket.on("update-board-name", function (req) {
+		const filter = {_id: mongoose.Types.ObjectId(req._id)};
+		const update = {name: req.name};
 
-        Board.findOneAndUpdate(filter, update, {new: true},
-            function (err) {
-                if (err) {
-                    console.log("Something wrong when updating board!");
-                }
-                io.to(board).emit("board-name-update", JSON.stringify({
-                    name: req.name
-                }));
-            }
-        )
-    });
+		Board.findOneAndUpdate(filter, update, {new: true},
+			function (err) {
+				if (err) {
+					console.log("Something wrong when updating board!");
+				}
+				io.to(board).emit("board-name-update", JSON.stringify({
+					name: req.name
+				}));
+			}
+		);
+	});
 
-    socket.on('delete-board', function(req) {
-        const card_filter = {boardId: mongoose.Types.ObjectId(req._id)};
-        const board_filter = {_id: mongoose.Types.ObjectId(req._id)};
-        
-        Card.deleteMany(card_filter, function(err) {
-            if (err) {
-                console.log("Cannot delete the cards in this board");
-            }
-        });
-        
-        Board.deleteOne(board_filter, function(err) {
-            if (err) {
-                console.log("Cannot delete this board");
-            }
-            io.to(board).emit("board-deleted");
-        });
-    });
+	socket.on("delete-board", function(req) {
+		const card_filter = {boardId: mongoose.Types.ObjectId(req._id)};
+		const board_filter = {_id: mongoose.Types.ObjectId(req._id)};
+
+		Card.deleteMany(card_filter, function(err) {
+			if (err) {
+				console.log("Cannot delete the cards in this board");
+			}
+		});
+
+		Board.deleteOne(board_filter, function(err) {
+			if (err) {
+				console.log("Cannot delete this board");
+			}
+			io.to(board).emit("board-deleted");
+		});
+	});
 
 });
-
