01eb785b0aa8ce6b309942d2447cd0a1d0014483
akeller <annina.keller@visablelabs.com>
2020-06-03 14:17:38 +0200

    make it work

--- controller/app_controller.js
+++ controller/app_controller.js
@@ -1,7 +1,7 @@
 var app = require("../main"),
 	faker = require("faker");
 
-exports.get_port = function() {
+exports.get_port = function () {
 	return app.get("port");
 };
 
@@ -9,7 +9,7 @@ exports.set_username = (req, res) => {
 	var newName = req.query.username;
 	res.cookie("username", newName);
 	res.locals.username = newName;
-	res.json( {status: "200", username: newName});
+	res.json({status: "200", username: newName});
 };
 
 exports.get_username = (req, res, next) => {
@@ -18,27 +18,24 @@ exports.get_username = (req, res, next) => {
 	next();
 };
 
-function generateUsername(req, res){
+function generateUsername(req, res) {
 	var cookie = req.cookies.username;
 	if (cookie === undefined) {
 		cookie = faker.name.findName();
 		res.cookie("username", cookie);
 		req.cookies.username = cookie;
-	}
-	else {
+	} else {
 		console.log("cookie exists", cookie);
 	}
 	res.locals.username = cookie;
 }
 
-
-
-function shuffle(array){
-	for(let i = array.length - 1; i > 0; i--){
-	  const j = Math.floor(Math.random() * i);
-	  const temp = array[i];
-	  array[i] = array[j];
-	  array[j] = temp;
+function shuffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * i);
+		const temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 	}
 	return array;
 }
--- models/card.js
+++ models/card.js
@@ -1,23 +1,32 @@
 const mongoose = require("mongoose"),
-    cardSchema = mongoose.Schema({
-        _id: mongoose.ObjectId,
-        backgroundColor: String,
-        position: {
-            left: Number,
-            top: Number
-        },
-        text: String,
-        fontSize: Number,
-        shape: {
-            type: String,
-            enum: ["RECTANGLE", "CIRCLE", "TRIANGLE", "ELLIPSE"],
-            default: "RECTANGLE"
-        },
-        boardId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Board"
-        },
-        comments: [{sender: String, message: String}]
-    });
+	cardSchema = mongoose.Schema({
+		_id: mongoose.ObjectId,
+		backgroundColor: String,
+		position: {
+			left: Number,
+			top: Number
+		},
+		text: String,
+		fontSize: Number,
+		shape: {
+			type: String,
+			enum: ["RECTANGLE", "CIRCLE", "TRIANGLE", "ELLIPSE"],
+			default: "RECTANGLE"
+		},
+		boardId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Board"
+		},
+		comments: [{sender: String, message: String}],
+		linkId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Board"
+		},
+		type: {
+			type: String,
+			enum: ["LINK", "NORMAL"],
+			default: "NORMAL"
+		},
+	});
 
 module.exports = mongoose.model("Card", cardSchema);
