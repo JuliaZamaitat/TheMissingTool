d643ad6ff8238b363a39a73ba0cac78947bddb74
ledlinh <ledlinh1107@gmail.com>
2020-06-17 17:27:38 +0200

    Chat window, comments remade
    TODO: comments as task, create board link with new card animation, card link restyle

--- public/script/client_sockets.js
+++ public/script/client_sockets.js
@@ -54,11 +54,25 @@ function createCard(data) {
 		card.style.backgroundColor = "transparent";
 	}
 
+	card.innerHTML = "<textarea type='text' value=''></textarea>";
+
 	const buttons = document.createElement("div");
 	buttons.className = "neu-float-panel buttonContainer";
-	buttons.innerHTML = "<span type='button' class='neu-button plain link'><img src='/icons/link.webp'></span><span type='button' class='neu-button plain colorChangeBtn'><div class='colorChangeOptions'></div><img src='/icons/palette.webp'></span><span type='button' class='neu-button plain commentBtn'><img src='/icons/comment.webp'></span><span type='button' class='neu-button plain deleteBtn'><img src='/icons/bin.webp'></span>";
-	card.innerHTML = "<textarea type='text' value=''></textarea><div class='comments-box'><span class='close-commentBox'>&times;</span><div class='commentField'></div><input placeholder='Add a comment...' class='commentInput'></div>";
+	buttons.innerHTML = "<span type='button' class='neu-button plain link'><img src='/icons/link.svg'></span><span type='button' class='neu-button plain colorChangeBtn'><div class='colorChangeOptions'></div><img src='/icons/palette.svg'></span><span type='button' class='neu-button plain commentBtn'><img src='/icons/comment.svg'></span><span type='button' class='neu-button plain deleteBtn'><img src='/icons/bin.svg'></span>";
+	// einen neuen resizeBtn in buttons hinzufuegen
+
+	const commentBox = document.createElement("div");
+	commentBox.className = "comments-box neu-float-panel";
+	commentBox.innerHTML = "<div class='comments-box_header'>Comments<span class='close-commentBox'>&times;</span></div><div class='commentField flex-container'></div><input placeholder='Type a comment...' class='commentInput neu-input'>";
+	
+	// const resizeGroup = document.createElement("div");
+	// resizeGroup.className = "resize-buttons_group";
+	// resizeGroup.innerHTML = "...";
+
+
 	card.prepend(buttons);
+	card.append(commentBox);
+	// card.append(resizeGroup);
 
 	card.id = data._id;
 	assignColorsToChange(card);
@@ -76,10 +90,9 @@ function createCard(data) {
 	}
 
 	let comments = data.comments;
-	for (let i = comments.length - 1; i > 0; i--) {
-		const comment = document.createElement("div");
-		comment.innerHTML = "<div class='comment-container'><p class='senderName'>" + comments[i].sender + "</p><p class='commentMessage'>" + comments[i].message + "</p><p class='timestamp'>" + new Date(comments[i].timestamp).toGMTString() + "</p></div>";
-		card.querySelector(".commentField").appendChild(comment);
+	for (let i = 0; i < comments.length  ; i++) {
+		const commentContainer  = addComment(comments[i]);
+		card.querySelector(".commentField").prepend(commentContainer);
 	}
 
 	if (data.linkId !== null && data.linkId !== undefined) {
@@ -99,14 +112,15 @@ function createCard(data) {
 }
 
 function convertToLink(card) {
+	card.classList.add("link-card");
 	const link = card.querySelector(".link");
 	if (card.querySelector(".link"))
 		link.remove();
-	const element = document.createElement("span");
-	element.className = "forward";
+	const element = document.createElement("button");
 	element.type = "button";
-	element.innerHTML = "<i class='fa fa-arrow-right'></i>";
-	card.append(element);
+	element.className = "forward";
+	element.innerHTML = "<img src='/icons/link.svg'>";
+	card.appendChild(element);
 	// Listener for forwarding to new board
 	card.querySelector(".forward").addEventListener("mousedown", function () {
 		$.get("/get-linked-board/" + card.id, function (data) {
@@ -118,7 +132,6 @@ function convertToLink(card) {
 		});
 	});
 }
-
 
 function addListeners(card, data) {
 	// Moving card listener
@@ -215,7 +228,7 @@ function addListeners(card, data) {
 		socket.emit("delete-card", cardToDelete);
 	});
 
-	// Show chat
+	// Show comment
 	card.querySelector(".commentBtn").addEventListener("mousedown", function () {
 		$("#" + card.id + " .comments-box").fadeIn();
 	});
@@ -267,14 +280,9 @@ $("#board-name").on("input", function (event) {
 });
 
 $("#delete-board").on("click", deleteBoard);
-$("#export-board").on("click", exportBoard);
 
 function deleteBoard() {
 	socket.emit("delete-board", {_id: window.windowBoardId});
-}
-
-function exportBoard() {
-	// TODO
 }
 
 // event listener for toolbar buttons
@@ -360,8 +368,6 @@ socket.on("card-to-link", (data) => {
 
 socket.on("board-name-update", (data) => {
 	const name = JSON.parse(data).name;
-	// $("#board-name").val(name);
-	// $("#board-name").css("width", name.length / 1.5 + "rem");
 });
 
 $("#user-name").on("focusout", function (event) {
@@ -375,9 +381,8 @@ socket.on("board-deleted", (data) => {
 });
 
 socket.on("comment", (data) => {
-	const comment = document.createElement("div");
-	comment.innerHTML = "<p class='senderName'>" + data.sender + "</p><p class='commentMessage'>" + data.message + "</p><p class='timestamp'>" + new Date(data.timestamp).toGMTString() + "</p>";
-	document.getElementById(data.cardId).querySelector(".commentField").appendChild(comment);
+	const commentContainer = addComment(data);
+	document.getElementById(data.cardId).querySelector(".commentField").prepend(commentContainer);
 });
 
 socket.on("display-card", (data) => {
@@ -393,15 +398,42 @@ socket.on("message", addMessage);
 
 function addMessage(message) {
 	$("#messageCount").text(( ++messageCount).toString());
+	$("#chat-hint").fadeOut();
 	let usernameEl = $("<b>").text(message.username);
 	let time = new Date(message.time);
-	let timeEl = $("<span>", {class: "text-secondary float-right"}).text(time.getHours() + ":" + time.getMinutes());
-	let messageHeadEl = $("<small>", {class: "messageHead"}).append(usernameEl, timeEl);
+	let timeEl = $("<small>", {class: "text-secondary float-right"}).text(formatHours(time.getHours(), time.getMinutes()));
+	let messageHeadEl = $("<div>", {class: "messageHead"}).append(usernameEl, timeEl);
 	let messageTextEl = $("<div>", {class: "messageText"}).text(message.text);
-	let messageEl = $("<div>", {class: "message mt-2"}).append(messageHeadEl, messageTextEl);
+	let messageEl = $("<div>", {class: "message"}).append(messageHeadEl, messageTextEl);
 	$("#chatContent").prepend(messageEl);
 	chatRescaleContent();
 	chatScrollBottom();
+}
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+function addComment(comment) {
+	const commentContainer = document.createElement("div");
+	commentContainer.className = "comment-container";
+	const date = new Date(comment.timestamp);
+	const dateString = months[date.getMonth() - 1] + " " + add0(date.getDate()) + ", " + date.getFullYear() + " at " + formatHours(date.getHours(), date.getMinutes());
+	commentContainer.innerHTML = "<p class='senderName'>" + comment.sender + ":</p><p class='commentMessage'>" + comment.message + "</p><p class='timestamp'>" + dateString + "</p>";
+	return commentContainer;
+}
+
+function add0(val) {
+	if (val < 10) {
+		return "0" + val;
+	} else {
+		return val;
+	}
+}
+
+function formatHours(hour, minutes) {
+	if (hour > 12) {
+		return add0(hour-12) + ":" + add0(minutes) + "pm";
+	} else {
+		return add0(hour) + ":" + add0(minutes) + "am";
+	}
 }
 
 function getRandomColor() {
@@ -447,13 +479,13 @@ function typingTimeout() {
 //listen for keypress in chatinput and emits typing
 $(document).ready(function () {
 
-
 	socket.emit("join", {boardId: window.windowBoardId, name: cookieValue("username")});
 
 	let currentBoards = cookieValue("visitedBoards");
 	if (currentBoards !== null) {
 		const arrayOfVisitedBoards = currentBoards.toString().split(",");
 		if (arrayOfVisitedBoards !== null && arrayOfVisitedBoards !== undefined) {
+			$("#folder").fadeIn();
 			arrayOfVisitedBoards.forEach(element => {
 				if (element !== null && element !== window.windowBoardId) {
 					appendNameToBoardList(element);
