b92e80ab8bab94b67dbb7b0b0d272b6c1bd99faf
akeller <annina.keller@visablelabs.com>
2020-07-03 12:12:45 +0200

    fix test

--- cypress/integration/chat/chat_spec.js
+++ cypress/integration/chat/chat_spec.js
@@ -6,7 +6,7 @@ describe("The Chat", () => {
 	});
 
 	it("shows the chat header", () => {
-		cy.get("#chatHeader").should("contain", "Ananas");
+		cy.get("#chatHeader").should("contain", "Chat");
 	});
 	it("does not show hidden chat content", () => {
 		cy.get("#chatContent").should("be.not.visible");
