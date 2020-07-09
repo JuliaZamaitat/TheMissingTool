cea07c3a1456210d9ea05a6d6f061f405569b55d
jannisbautsch <j.bautsch@gmx.de>
2020-07-09 12:20:41 +0200

    test chat functions

--- cypress/integration/chat/chat_spec.js
+++ cypress/integration/chat/chat_spec.js
@@ -5,28 +5,63 @@ describe("The Chat", () => {
 		cy.visit("/board/56cb91bdc3464f14678934ca");
 	});
 
-	it("shows the chat header", () => {
-		cy.get("#chatHeader").should("contain", "Chat");
+	it("shows the open-chat button", () => {
+		cy.get("#open-chat").should("be.visible");
 	});
-	it("does not show hidden chat content", () => {
-		cy.get("#chatContent").should("be.not.visible");
-	});
-	it("shows chat content after click on open chat button", () => {
+
+	it("shows chat window after click on open chat button", () => {
 		cy.get("#open-chat").click();
-		cy.get("#chatContent").should("be.visible");
+		cy.get("#chatWindow").should("be.visible");
+		cy.get("#chatHeader").should("be.visible");
+		cy.get("#chatBody").should("be.visible");
 	});
-	it("hides chat content after click on chat Header", () => {
-		cy.get("#chatHeader").click();
-		cy.get("#chatContent").should("be.visible");
+
+	it("chatHeader contains content", () => {
+		cy.get("#user-list-button").should("be.visible");
+		cy.get("#close-chat").should("be.visible");
 	});
+
+	it("chatBody contains content", () => {
+		cy.get("#chatContent").should("be.visible");
+		cy.get("#chatInputContainer").should("be.visible");
+	});
+
+	it("shows user list", () => {
+		cy.get("#user-list-button").trigger("mouseover");
+		cy.get(".pop-user-list").should("be.visible");
+		//cy.get(".pop-user-list").should("contain", "Francesco Russel");
+	});
+
 	it("appends messages to chat", () => {
 		cy.get("#chatContent").should("contain", "Hanz Franz");
 		cy.get("#chatContent").should("contain", "Some message");
 		cy.get("#chatContent").should("not.contain", "Some other username");
 		cy.get("#chatContent").should("not.contain", "Some other message");
 	});
+
+	it("send new message", () => {
+		cy.get("#chatInput").type("hello, world! {enter}");
+		cy.get("#chatContent").should("contain", "hello, world!");
+	});
+
 	it("hides chat content when clicking on close button", () => {
 		cy.get("#close-chat").click();
 		cy.get("#chatContent").should("be.not.visible");
 	});
+
+	/*
+	it("shows the chat header", () => {
+		cy.get("#chatHeader").should("contain", "Chat");
+	});
+	it("does not show hidden chat content", () => {
+		cy.get("#chatContent").should("be.not.visible");
+	});
+	it("hides chat content after click on chat Header", () => {
+		cy.get("#chatHeader").click();
+		cy.get("#chatContent").should("be.visible");
+	});
+
+
+
+	 */
 });
